a251db1cdf8d64b777af4cb80d3abd3bfaf0ef30
Vasily Mitronov <vasmitr95@gmail.com>
2023-07-24 15:07:00 +0000

    Add sessions

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var session = require('express-session');
+
+
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 
 var app = express();
 
 app.locals.pluralize = require('pluralize');
+
+var SQLiteStore = require('connect-sqlite3')(session);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +28,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
